5095b3e274e452161e8aa744fa982bc7b8325264
ivanVillalobs <a21310394@ceti.mx>
2023-12-03 20:29:41 -0600

    Inventario listo

--- Proyecto3/app/src/main/java/com/example/proyecto/ui/inventarioAdm/InventarioAdmFragment.kt
+++ Proyecto3/app/src/main/java/com/example/proyecto/ui/inventarioAdm/InventarioAdmFragment.kt
@@ -1,44 +1,45 @@
 package com.example.proyecto.ui.inventarioAdm
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import com.example.proyecto.Ingrediente
 import com.example.proyecto.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class InventarioAdmFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventario_adm, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_inventario_adm, container, false)
 
-    companion object {
+        val listaIngredientes = mutableListOf(
+            Ingrediente(1, "Ingrediente 1", 100),
+            Ingrediente(2, "Ingrediente 2", 200),
+            Ingrediente(3, "Ingrediente 3", 300),
+            Ingrediente(4, "Ingrediente 4", 400),
+            Ingrediente(5, "Ingrediente 5", 500),
+            Ingrediente(6, "Ingrediente 6", 600),
+            Ingrediente(7, "Ingrediente 7", 700),
+            Ingrediente(8, "Ingrediente 8", 800),
+            Ingrediente(9, "Ingrediente 9", 900),
+            Ingrediente(10, "Ingrediente 10", 1000)
+        )
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InventarioAdmFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val arrayAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            listaIngredientes
+        )
+
+        val listView: ListView = view.findViewById(R.id.listView)
+        listView.adapter = arrayAdapter
+
+        return view
     }
 }
